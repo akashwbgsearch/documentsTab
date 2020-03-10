7711de2f3f736aef1fd4f586c7c91168450cb21b
akashwbgsearch <ssivaprakasam@worldbankgroup.org>
2020-03-10 15:55:34 +0530

    initial commit

---
+++ src/app/excel.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
+
+@Injectable()
+export class ExcelService {
+
+  constructor() { }
+
+  public exportProjectsExcelFile(json: any[], excelFileName: string, tableHeaders: any[]): void {
+   
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+
+	worksheet['A1'].v = tableHeaders[0];
+	worksheet['B1'].v = tableHeaders[1];
+	worksheet['C1'].v = tableHeaders[2];
+	worksheet['D1'].v = tableHeaders[3];	
+
+	const workbook: XLSX.WorkBook = { Sheets: { 'Project Documents': worksheet }, SheetNames: ['Project Documents'] };
+	
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    this.saveAsExcelFile(excelBuffer, excelFileName);
+	
+  }
+  
+    public exportArchivalExcelFile(json: any[], excelFileName: string, tableHeaders: any[]): void {
+   
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+
+	worksheet['A1'].v = tableHeaders[0];
+	worksheet['B1'].v = tableHeaders[1];
+	worksheet['C1'].v = tableHeaders[2];
+	worksheet['D1'].v = tableHeaders[3];	
+
+	const workbook: XLSX.WorkBook = { Sheets: { 'Archival Records': worksheet }, SheetNames: ['Archival Records'] };
+	
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    this.saveAsExcelFile(excelBuffer, excelFileName);
+	
+  }
+
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(data, fileName + EXCEL_EXTENSION);
+  }
+
+}
